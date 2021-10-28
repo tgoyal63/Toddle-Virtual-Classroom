c6bf23fdf59de0391857d70f025cb3f7b0a8fe1c
Tushar Goyal <tgoyal63@gmail.com>
2021-10-28 11:18:49 +0530

    Added Documentation Link

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({
  * App Routes
  */
 app.get('/', (req, res) => {
-  res.send('Welcome to Virtual Classroom. You can access to our documentation by visiting ...');
+  res.send('Welcome to Virtual Classroom. You can access to our documentation by visiting https://documenter.getpostman.com/view/10110440/UV5deEZQ');
 });
 
 app.use('/', userRoutes);
